f320e37f8faaba76f506b6b9e38b1201e8645093
marinaAC <maru.cardozo02@gmail.com>
2021-08-26 22:23:20 -0300

    primer ejercicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'ejercicio-uno';
+  title = 'practicas primera clase';
+
+  edadUno?:number;
+  edadDos?:number;
+  suma?:number;
+  promedio?:number | string;
+
+
+  calcularPromedio(){
+    if(this.edadUno !== undefined && this.edadDos !== undefined){
+        this.suma = this.edadUno + this.edadDos;
+        this.promedio = this.validarSiEsCero(this.edadDos) ? "No se puede dividir por cero" : this.suma / 2 ;
+    }
+  }
+
+  limpiar(){
+      this.edadUno = undefined;
+      this.edadDos = undefined;
+      this.promedio = undefined;
+      this.suma = undefined;
+  }
+
+  private validarSiEsCero(number:number):boolean{
+    return number === 0;
+  }
 }
